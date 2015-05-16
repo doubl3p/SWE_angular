7229f46598f01adf7f16bc1507971966086e74d8
Paul Pelludat <pelludat.paul@googlemail.com>
2015-05-16 20:27:27 +0200

    Bootstrap eingebunden und Router erste Route-Stati

--- app/base/app.js
+++ app/base/app.js
@@ -7,26 +7,27 @@
     'use strict';
 
     angular.module('webapp',
-        ['startpage', 'ui.router']);
-        // angular Services werden zweimal angegeben. Da Parameter bei Minifien z.B. zu a und b werden
-        // Parameter kann man benennen wir man mag
-        //.config('$urlRouterProvider', 'StateProvider', function($urlRouterProvider, $statProvider){
-        //
-        //    //wenn man hinter '#/' etwas eingibt, dass es nicht gibt wird hier hin geroutet
-        //    $urlRouterPrivider.otherwise("/");
-        //
-        //    $stateProvider
-        //        .state("home", {
-        //            url: '/',
-        //            templateUrl: '../templates/home.html'
-        //        })
-        //        .state("about", {
-        //            url: '/about',
-        //            templateUrl: '../templates/about.html'
-        //        });
-        //
-        //
-        //});
+        ['startpage', 'ui.router'])
+
+         //angular Services werden zweimal angegeben. Da Parameter bei Minifien z.B. zu a und b werden
+         //Parameter kann man benennen wir man mag
+        .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+
+            //wenn man hinter '#/' etwas eingibt, dass es nicht gibt wird hier hin geroutet
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'templates/home.html'
+                })
+                .state('about', {
+                    url: '/about',
+                    templateUrl: 'templates/about.html'
+                });
+
+
+        }]);
 
 
 })();
