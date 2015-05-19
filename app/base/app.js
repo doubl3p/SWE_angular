df1b2e61333b2a6b7965dceba59b4aeed8e58a02
Paul Pelludat <pelludat.paul@googlemail.com>
2015-05-19 15:19:01 +0200

    #7 edited navigation and router

--- app/base/app.js
+++ app/base/app.js
@@ -10,27 +10,42 @@
         ['home', 'ui.router'])
 
          //angular Services werden zweimal angegeben. Da Parameter bei Minifien z.B. zu a und b werden
-         //Parameter kann man benennen wir man mag
+         //Parameter kann man benennen wie man mag
         .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
 
             //wenn man hinter '#/' etwas eingibt, dass es nicht gibt wird hier hin geroutet
             $urlRouterProvider.otherwise('/');
 
             $stateProvider
-                .state('home', {
-                    url: '/',
-                    templateUrl: 'templates/home.html',
-                    controller: 'homeCtrl'
+                .state('artikel', {
+                    url: '/artikel',
+                    templateUrl: 'aritkelverwaltung/artikelverwaltung.html',
+                    controller: 'artikelVerwCtrl'
 
                 })
+
+                .state('meinKonto', {
+                    url: '/meinKonto',
+                    templateUrl: 'kundenverwaltung/kundenverwaltung.html',
+                    controller: 'kundenVerwCtrl'
+                })
+
+                .state('bestellung', {
+                    url: '/meinKonto/bestellung',
+                    templateUrl: 'bestellverwaltung/bestellverwaltung.html',
+                    controller: 'bestellVerwCtrl'
+                })
+
+                .state('warenkonto', {
+                    url: '/warenkonto',
+                    templateUrl: 'kundenverwaltung/kundenverwaltung.html',
+                    controller: 'bestellVerwCtrl'
+                })
+
                 .state('about', {
                     url: '/about',
                     templateUrl: 'templates/about.html',
                     controller: 'aboutCtrl'
-                })
-                .state('contact', {
-                    url: '/contact',
-                    templateUrl: 'templates/contact.html'
                 });
 
         }]);
